8b8d9872b30d3ac133b48a7876372d795791b216
btotharye <brianhh1230@gmail.com>
2018-06-11 16:16:27 -0400

    updating to work with more info context

--- __init__.py
+++ __init__.py
@@ -9,6 +9,10 @@ class YelpRestaurant(MycroftSkill):
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(YelpRestaurant, self).__init__(name="YelpRestaurant")
+        self.restaurant_phone = ''
+        self.restaurant_address = ''
+        self.rating = ''
+        self.is_open = False
 
     # This handle is used to lookup a restaurant near the person's location
     @intent_handler(IntentBuilder("")
@@ -30,14 +34,47 @@ class YelpRestaurant(MycroftSkill):
                                                limit='5',
                                                sort_by='best_match')
         restaurant_name = search_results['businesses'][0]['name']
+        restaurant_phone = search_results['businesses'][0]['phone']
+        restaurant_rating = search_results['businesses'][0]['rating']
+        restaurant_location = search_results['businesses'][0]['location']['display_address'][0] + \
+                              " " + search_results['businesses'][0]['location']['display_address'][1]
+        restaurant_open = search_results['businesses'][0]['is_closed']
+        self.set_context('RestaurantName', restaurant_name)
+        self.restaurant_phone = restaurant_phone
+        self.rating = restaurant_rating
+        self.restaurant_address = restaurant_location
+        self.is_closed = restaurant_open
         rating = search_results['businesses'][0]['rating']
         print(search_results['businesses'][0])
         self.speak_dialog("restaurant", data={
                           "restaurant_name": restaurant_name,
                           "rating": rating})
 
+    @intent_handler(IntentBuilder("")
+                    .require('RestaurantName')
+                    .require('MoreInformation'))
+    def handle_more_info(self, message):
+        print(message.data)
+        restaurant_name = message.data.get('RestaurantName')
+        restaurant_phone = self.restaurant_phone
+        restaurant_location = self.restaurant_address
+        if self.is_closed is True:
+            restaurant_open = 'closed'
+        else:
+            restaurant_open = 'open'
+
+        restaurant_rating = self.rating
+        print("This is the restaurant name: {}".format(restaurant_name))
+        print("This is the restaurant phone: {}".format(restaurant_phone))
+        self.speak_dialog("moreinfo", data={
+                          "restaurant_name": restaurant_name,
+                          "phone_number": restaurant_phone,
+                          "address": restaurant_location,
+                          "is_closed": restaurant_open})
+
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
     return YelpRestaurant()
+
