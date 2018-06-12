058e42c50125089eb2443185147454f358f166d4
btotharye <brianhh1230@gmail.com>
2018-06-12 17:51:18 -0400

    adding next result/restaurant

--- __init__.py
+++ __init__.py
@@ -13,6 +13,8 @@ class YelpRestaurant(MycroftSkill):
         self.restaurant_address = ''
         self.rating = ''
         self.is_closed = False
+        self.json_response = ''
+        self.index = 0
 
     # This handle is used to lookup a restaurant near the person's location
     @intent_handler(IntentBuilder("")
@@ -31,20 +33,21 @@ class YelpRestaurant(MycroftSkill):
                                                longitude=longitude,
                                                limit='5',
                                                sort_by='best_match')
-        restaurant_name = search_results['businesses'][0]['name']
-        restaurant_phone = search_results['businesses'][0]['phone']
-        restaurant_rating = search_results['businesses'][0]['rating']
-        restaurant_location = search_results['businesses'][0]['location']['display_address'][0] + \
+        businesses = search_results['businesses'][self.index]
+        restaurant_name = businesses['name']
+        restaurant_phone = businesses['phone']
+        restaurant_rating = businesses['rating']
+        restaurant_location = businesses['location']['display_address'][0] + \
             " " + \
-            search_results['businesses'][0]['location']['display_address'][1]
-        restaurant_open = search_results['businesses'][0]['is_closed']
+            businesses['location']['display_address'][1]
+        restaurant_open = businesses['is_closed']
+        self.json_response = search_results
         self.set_context('RestaurantName', restaurant_name)
         self.restaurant_phone = restaurant_phone
         self.rating = restaurant_rating
         self.restaurant_address = restaurant_location
         self.is_closed = restaurant_open
-        rating = search_results['businesses'][0]['rating']
-        print(search_results['businesses'][0])
+        rating = businesses['rating']
         self.speak_dialog("restaurant", data={
                           "restaurant_name": restaurant_name,
                           "rating": rating})
@@ -53,10 +56,14 @@ class YelpRestaurant(MycroftSkill):
                     .require('RestaurantName')
                     .require('MoreInformation'))
     def handle_more_info(self, message):
-        restaurant_name = message.data.get('RestaurantName')
-        restaurant_phone = self.restaurant_phone
-        restaurant_location = self.restaurant_address
-        if self.is_closed is True:
+        json_response = self.json_response
+        businesses = json_response['businesses'][int(self.index)]
+        restaurant_name = businesses['name']
+        restaurant_phone = businesses['phone']
+        restaurant_location = businesses['location']['display_address'][0] + \
+                              " " + \
+                              businesses['location']['display_address'][1]
+        if businesses['is_closed'] is True:
             restaurant_open = 'closed'
         else:
             restaurant_open = 'open'
@@ -66,6 +73,29 @@ class YelpRestaurant(MycroftSkill):
                           "address": restaurant_location,
                           "is_closed": restaurant_open})
 
+    @intent_handler(IntentBuilder("")
+                    .require("NextResult"))
+    def handle_next_result(self, message):
+        self.index += 1
+        print(self.index)
+        json_response = self.json_response
+        print("This is the json response: {}".format(json_response))
+        try:
+            businesses = json_response['businesses'][int(self.index)]
+        except IndexError as e:
+            self.speak("Sorry there are no more results.")
+        restaurant_name = businesses['name']
+        restaurant_phone = businesses['phone']
+        restaurant_location = businesses['location']['display_address'][0] + \
+            " " + \
+            businesses['location']['display_address'][1]
+        if businesses['is_closed'] is True:
+            restaurant_open = 'closed'
+        else:
+            restaurant_open = 'open'
+        self.speak_dialog("next.result", data={
+                          "restaurant_name": restaurant_name,
+                          "rating": businesses['rating']})
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
